6f9d6c26fa0e302b2300d9a927f7bd77426c8092
王福元 <wangfuyuan@nnuo.com>
2023-01-31 11:36:07 +0800

    chore: 添加完整网站

---
+++ gatsby-config.ts
@@ -0,0 +1,63 @@
+import type { GatsbyConfig, PluginRef } from "gatsby";
+import "dotenv/config";
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitle: `我是福元`,
+    siteTitleAlt: `我是福元 - 做自己喜欢的事儿！`,
+    siteHeadline: `我是福元 - 做自己喜欢的事儿！`,
+    siteUrl: `https://ifuyuan.wang`,
+    siteDescription: `我是福元-我希望能够做自己喜欢的事情！！！`,
+    siteImage: `/banner.jpg`,
+    author: `@lekoarts_de`,
+  },
+  trailingSlash: `never`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `我是福元`,
+        short_name: `我是福元`,
+        description: `我是福元-我希望能够做自己喜欢的事情！！！`,
+        start_url: `/`,
+        background_color: `#141821`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+};
+
+export default config;
