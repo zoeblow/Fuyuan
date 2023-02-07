aadab341e494e2b0dd3689bd05149f344b7dc2f5
王福元 <wangfuyuan@nnuo.com>
2023-02-07 11:36:42 +0800

    chore: 修改footer

---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,22 @@
+/*
+ * @Description:
+ * @Author: wangfuyuan
+ * @Email: wangfuyuan@nnuo.com
+ * @Date: 2023-02-07 11:25:01
+ * @LastEditors: wangfuyuan
+ * @LastEditTime: 2023-02-07 11:34:07
+ * @FilePath: \Fuyuan\src\@lekoarts\gatsby-theme-cara\components\footer.tsx
+ */
+/** @jsx jsx */
+import { Box, jsx } from "theme-ui"
+
+const Footer = () => {
+
+  return (
+    <Box as="footer" variant="footer">
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+    </Box>
+  )
+}
+
+export default Footer
